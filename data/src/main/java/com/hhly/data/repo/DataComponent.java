e93a526b1cba65cee0827a12ad8c0108610e7025
xuliang241 <xul@13322.com>
2017-04-25 11:42:15 +0800

    更改android-apt 为 annotationProcessor

--- data/src/main/java/com/hhly/data/repo/DataComponent.java
+++ data/src/main/java/com/hhly/data/repo/DataComponent.java
@@ -1,7 +1,5 @@
 package com.hhly.data.repo;
 
-import com.hhly.data.api.BaseApi;
-
 import javax.inject.Singleton;
 
 import dagger.Component;
@@ -11,8 +9,9 @@ import dagger.Component;
  * 作    者：xul@13322.com
  * 时    间：2016/9/7
  */
-@Singleton
+
 @Component(modules = {DataModule.class})
+@Singleton
 public interface DataComponent {
 
     void inject(DataManager dataManager);
